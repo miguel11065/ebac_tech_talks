2bfe5b107b3f1ad0125ab8f04c40d90d0fd5de99
Miguel Leal <miguel11065@gmail.com>
2023-07-24 20:41:05 -0300

    criando contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,31 @@
-console.log("ola mundo");
+AOS.init();
+
+const dataDoEvento = new Date("Aug 12, 2023 19:00:00");
+const timeStampEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutossAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `começa em ${diasAteOEvento}d ${horasAteOEvento}h ${minutossAteOEvento}min ${segundosAteOEvento}s`;
+    document.getElementById('button').innerHTML = `Garanta sua vaga`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'terminou!';
+        document.getElementById('button').innerHTML = 'Ingresso esgotado';
+    }
+
+    //if (distanciaAteOEvento)
+},1000);
